df0fef7966e8b87e14f03a719889943621ccdc3b
Likhan Siddiquee <likhan.siddiquee@gmail.com>
2025-11-05 15:51:45 +0100

    improve test coverage

--- jest.config.js
+++ jest.config.js
@@ -25,7 +25,11 @@ module.exports = {
   // Coverage collection configuration
   collectCoverageFrom: [
     '**/*.ts',
-    '!**/index.ts',
+    '!**/index.ts', // Index files are re-exporters
+    '!drivers/**/device.ts', // Devices are Homey specific entry points but they are thin wrappers
+    '!drivers/**/driver.ts', // Drivers are Homey specific entry points but they are thin wrappers
+    '!utils/*Logger.ts',
+    '!utils/ApiPollingManager.ts', // Explicitly excluding as not being used yet
     '!**/*.test.ts',
     '!**/*.d.ts',
     '!**/node_modules/**',
